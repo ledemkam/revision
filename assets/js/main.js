141c15c9cf935c08e39f048c874f8f0114e9fecb
ledemkam <ledemkam@gmail.com>
2022-03-10 06:54:03 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+// tu passes une fonciton et cette fonction va loop over ton array pour chercher ce que tu veux
+// on prends inventor on loop over et pô
+
+const fifteen = inventors.filter(function (inventor) {
+  if (inventor.year >= 1500 && inventor.year < 1600) {
+    return true; // ça le garde
+  } else {
+    return false;
+  }
+});
+console.table(fifteen);
+// Array.prototype.map()
+// 2. Give us an array of the inventors' first and last names
+// map sert à donner un tableau de ce qu'on cherche
+const fullNames = inventors.map(
+  (inventor) => `${inventor.first} ${inventor.last}`
+);
+console.table(fullNames);
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+// sort c'est pour trier retourne un tableau
+const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+console.table(ordered);
+// Array.prototype.reduce()
+// traite chaque valeur d'une liste pour la réduire à une seule valeur, un accumulateur
+
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYears);
+
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+];
